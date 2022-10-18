e7ac837e0271783efcd80037c77823f97ddc0bdf
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-10-19 00:08:53 +1100

    complete GET deliverable

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  //useEffect to perform a side effect of fetching questions from the API when the component first renders
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then(res => res.json())
+      .then(questions => {
+        setQuestions(questions);
+        console.log("Questions after loading from API: ", questions);
+      });
+  }, []);
+
+  const questionsLi =questions.map(question => <QuestionItem key={question.id} question={question}></QuestionItem>)
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionsLi}</ul>
     </section>
   );
 }
